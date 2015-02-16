10b08d47fa81db681fc11c4c6fc4aa348b566ad2
jamesvoneiff <jamesvoneiff@163.11.237.223>
2015-02-16 17:12:08 -0500

    added tests for TinyE

--- src/edu/cedarville/cs/crypto/TinyE.java
+++ src/edu/cedarville/cs/crypto/TinyE.java
@@ -11,6 +11,27 @@ public class TinyE {
         ECB, CBC, CTR
     };
     private final String DELTA = "9E3779B9";
+    
+    public static void main(String[] args) {
+        Integer[] plain = {5,5,443,3,45};
+        Integer[] cipher = new Integer[plain.length];
+        Integer[] key1 = {42314,2345,27554,10};
+        Integer[] IV = {0};
+        Mode curMode = Mode.ECB;
+        TinyE tea = new TinyE();
+        cipher = tea.encrypt(plain, key1, curMode, IV);
+        for(Integer i = 0; i < cipher.length; i++){
+            System.out.print(cipher[i]);            
+        }
+        System.out.println("");
+        
+        plain = tea.decrypt(cipher, key1, curMode, IV);
+        for(Integer i = 0; i < plain.length; i++){
+            System.out.print(plain[i]);            
+        }
+        System.out.println("");        
+        
+    }
 
     public Integer[] encrypt(Integer[] plaintext, Integer[] key, Mode mode, Integer[] iv) {
         /* REMINDERS
@@ -39,25 +60,23 @@ public class TinyE {
          cipherText = (L,R);
          */
         
-        Integer[] ciphertext = {0};
-        Integer[] K = {0};
-        for(Integer k = 0; k < 3; k++) {
-            K[k] = key[k];
-        }
+        Integer[] ciphertext = new Integer[plaintext.length];
+
         Integer[] delta = convertFromHexStringToInts(DELTA);
+        System.out.println(delta.length);
         long sum = 0;
         if(mode == Mode.ECB) {
             for(Integer i = 0; i < plaintext.length; i+=2) {
-                Integer L = plaintext[i];
-                Integer R = plaintext[i+1];
+                long L = plaintext[i];
+                long R = plaintext[i+1];
                 for(Integer j = 0; j < 32; j++) {
                     sum += (((long) delta[0] << 32) & 0xffffffff00000000l)|((long)(delta[1] & 0x00000000ffffffffl));
-                    L += ((R << 4)+K[0])^(R+(int)sum)^((R >> 5)+K[1]);
-                    R += ((L << 4)+K[2])^(L+(int)sum)^((L >> 5)+K[3]);    
+                    L += ((R << 4)+key[0])^(R+sum)^((R >> 5)+key[1]);
+                    R += ((L << 4)+key[2])^(L+sum)^((L >> 5)+key[3]);    
                 }
                 sum = 0;
-                ciphertext[i] = L;
-                ciphertext[i+1] = R;
+                ciphertext[i] = (int)L;
+                ciphertext[i+1] = (int)R;
             }
         } else if(mode == Mode.CBC) {
             
@@ -87,26 +106,23 @@ public class TinyE {
          cipherText = {L,R};
          */
         
-        Integer[] plaintext = {0};
-        Integer[] K = {0};
-        for(Integer k = 0; k < 3; k++) {
-            K[k] = key[k];
-        }
+        Integer[] plaintext = new Integer[ciphertext.length];
+
         Integer[] delta = convertFromHexStringToInts(DELTA);
         long sum = (((long) delta[0] << 32) & 0xffffffff00000000l)|((long)(delta[1] & 0x00000000ffffffffl)) << 5;
         if(mode == Mode.ECB) {
             for(Integer i = 0; i < ciphertext.length; i+=2) {
-                Integer L = ciphertext[i];
-                Integer R = ciphertext[i+1];
+                long L = ciphertext[i];
+                long R = ciphertext[i+1];
                 for(Integer j = 0; j < 32; j++) {
                     sum -= (((long) delta[0] << 32) & 0xffffffff00000000l)|((long)(delta[1] & 0x00000000ffffffffl));
-                    R -= ((L << 4)+K[2])^(L+(int)sum)^((L >> 5)+K[3]);
-                    L -= ((R << 4)+K[0])^(R+(int)sum)^((R >> 5)+K[1]);
+                    R -= ((L << 4)+key[2])^(L+(int)sum)^((L >> 5)+key[3]);
+                    L -= ((R << 4)+key[0])^(R+(int)sum)^((R >> 5)+key[1]);
     
                 }
-                sum = 0;
-                plaintext[i] = L;
-                plaintext[i+1] = R;
+                sum = (((long) delta[0] << 32) & 0xffffffff00000000l)|((long)(delta[1] & 0x00000000ffffffffl)) << 5;
+                plaintext[i] = (int)L;
+                plaintext[i+1] = (int)R;
             }
         } else if(mode == Mode.CBC) {
             
