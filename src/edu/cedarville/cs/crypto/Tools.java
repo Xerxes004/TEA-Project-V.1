2c1b4b781f0237f2acb3fc973e7d60405886aa41
wes <wes@163.11.1.220>
2015-02-15 20:21:36 -0500

    Fixed it I think. Should convert from ints to bytes and vice versa.

--- src/edu/cedarville/cs/crypto/Tools.java
+++ src/edu/cedarville/cs/crypto/Tools.java
@@ -1,6 +1,5 @@
 package edu.cedarville.cs.crypto;
 
-import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
 public class Tools {
@@ -11,12 +10,12 @@ public class Tools {
         for (int i = 0; i < ints.length; i++) {
             int a,b,c,d;
             
-            a = (j   < bs.length ? bs[j] : 0) << 24;
-            b = (j+1 < bs.length ? bs[j+1] : 0) << 16 ;
-            c = (j+2 < bs.length ? bs[j+2] : 0) << 8 ;
+            a = (j   < bs.length ? bs[j]   : 0) << 24;
+            b = (j+1 < bs.length ? bs[j+1] : 0) << 16;
+            c = (j+2 < bs.length ? bs[j+2] : 0) << 8;
             d = (j+3 < bs.length ? bs[j+3] : 0);
             
-            ints[i] = a | b | c | d;
+            ints[i] = a | b | c | d ;
             
             j += 4;
         }
@@ -35,21 +34,18 @@ public class Tools {
 	}
 	
 	public static byte[] convertFromIntsToBytes(Integer[] ints) {
-        byte[] bytes = new byte[ints.length * 4];
-        for (int i = 0; i < bytes.length; i++) {
-            bytes[i] = 0xf;
-        }
-        int j = 0;
-        for (int i = 0; i < ints.length; i++) {
-            
-            bytes[j]   |= ints[i];
-            bytes[j+1] |= ints[i] >>> 8;
-            bytes[j+2] |= ints[i] >>> 16;
-            bytes[j+3] |= ints[i] >>> 24;
-            
-            j += 4;
-        }
-		return bytes;
+            byte[] bytes = new byte[ints.length * 4];
+            int j = 0;
+            for (int i = 0; i < ints.length; i++) {
+                
+                bytes[j]   |= ints[i];
+                bytes[j+1] |= ints[i] >>> 8;
+                bytes[j+2] |= ints[i] >>> 16;
+                bytes[j+3] |= ints[i] >>> 24;
+                
+                j += 4;
+            }
+            return bytes;
 	}
 	
 	public static String convertFromIntsToHexString(Integer[] ints) {
@@ -59,17 +55,19 @@ public class Tools {
     public static void main (String[] args) {
         String str = "Hello world!";
             
-        Integer[] testInt = {1};
+        Integer[] testInt = {0xffff};
 
-        System.out.println("Initial value: " + Integer.toBinaryString(testInt[0]));
-
+        System.out.println("Initial value 1: " + Integer.toHexString(testInt[0]));
+        
         byte[] bytes = convertFromIntsToBytes(testInt);
-        System.out.println("Converted to: " + Integer.toBinaryString(bytes[0]));
-
+        
+        System.out.println("Converted to: " + Integer.toHexString(bytes[0]));
+        
         testInt = convertFromBytesToInts(bytes);
-        System.out.println("End value: " + Integer.toBinaryString(testInt[0]));
-
-        int i = (((1 & 0xff) << 24) |
+        
+        System.out.println("End value: " + Integer.toHexString(testInt[0]));
+        
+        int i = (((1 & 0xff)   << 24) |
                  ((-10 & 0xff) << 16) |
                  ((-100 & 0xff) << 8) |
                  ((-1 & 0xff)));
