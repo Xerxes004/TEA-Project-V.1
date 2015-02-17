467821b2fbfbe8099748077b212f3a2d6d796d62
jjvoneiff <jjvoneiff@163.11.44.117>
2015-02-16 23:30:57 -0500

    Edited TinyE. added encryptions for CBC and CTR and decryptions for CBC and CTR.

--- src/edu/cedarville/cs/crypto/TinyE.java
+++ src/edu/cedarville/cs/crypto/TinyE.java
@@ -1,6 +1,7 @@
 package edu.cedarville.cs.crypto;
 
 import static edu.cedarville.cs.crypto.Tools.convertFromHexStringToInts;
+import static edu.cedarville.cs.crypto.Tools.convertFromIntsToHexString;
 
 
 
@@ -13,7 +14,7 @@ public class TinyE {
     private final String DELTA = "9E3779B9";
     
     public static void main(String[] args) {
-        Integer[] plain = {5,5,443,3,45,342};
+        Integer[] plain = {1234, 238, 4832, 3};
         Integer[] cipher = new Integer[plain.length];
         Integer[] key1 = {42314,2345,27554,10};
         Integer[] IV = {0};
@@ -27,10 +28,9 @@ public class TinyE {
         
         plain = tea.decrypt(cipher, key1, curMode, IV);
         for(Integer i = 0; i < plain.length; i++){
-            System.out.print(plain[i]);            
+            System.out.print(plain[i] + " ");            
         }
         System.out.println("");        
-        
     }
 
     public Integer[] encrypt(Integer[] plaintext, Integer[] key, Mode mode, Integer[] iv) {
@@ -59,29 +59,80 @@ public class TinyE {
          }
          cipherText = (L,R);
          */
+        for(int m = 0; m < plaintext.length; m++) {
+            System.out.print(plaintext[m] + " ");
+        }
+        System.out.println("");
         
         Integer[] ciphertext = new Integer[plaintext.length];
 
         Integer[] delta = convertFromHexStringToInts(DELTA);
-        System.out.println(delta[0]);
+        
         int sum = 0;
         if(mode == Mode.ECB) {
-            for(Integer i = 0; i < plaintext.length; i+=2) {
+            for(Integer i = 0; i < plaintext.length ; i+=2) {
                 int L = plaintext[i];
                 int R = plaintext[i+1];
                 for(Integer j = 0; j < 32; j++) {
                     sum += delta[0];
-                    L += ((R << 4)+key[0])^(R+sum)^((R >> 5)+key[1]);
-                    R += ((L << 4)+key[2])^(L+sum)^((L >> 5)+key[3]);    
+                    L += ((R << 4)+key[0])^(R+sum)^((R >>> 5)+key[1]);
+                    R += ((L << 4)+key[2])^(L+sum)^((L >>> 5)+key[3]);    
                 }
                 sum = 0;
                 ciphertext[i] = L;
                 ciphertext[i+1] = R;
             }
+            for(int k = 0; k < ciphertext.length; k++) {
+                System.out.print(ciphertext[k] + " ");
+            }
+            System.out.println("");
+            
         } else if(mode == Mode.CBC) {
+            Integer[] xorer = new Integer[2];
+            xorer[0] = iv[0];
+            xorer[1] = iv[1];
             
+            for(Integer i = 0; i < plaintext.length ; i+=2) {
+                int L = plaintext[i]^xorer[0];
+                int R = plaintext[i+1]^xorer[1];
+                for(Integer j = 0; j < 32; j++) {
+                    sum += delta[0];
+                    L += ((R << 4)+key[0])^(R+sum)^((R >>> 5)+key[1]);
+                    R += ((L << 4)+key[2])^(L+sum)^((L >>> 5)+key[3]);    
+                }
+                sum = 0;
+                xorer[0] = L;
+                xorer[1] = R;
+                ciphertext[i] = L;
+                ciphertext[i+1] = R;
+            }
+            for(int k = 0; k < ciphertext.length; k++) {
+                System.out.print(ciphertext[k] + " ");
+            }
+            System.out.println("");
+             
         } else {
+            Integer[] xorer = new Integer[2];
+            xorer[0] = iv[0];
+            xorer[1] = iv[1];
             
+            for(Integer i = 0; i < plaintext.length ; i+=2) {
+                int L = xorer[0];
+                int R = xorer[1];
+                for(Integer j = 0; j < 32; j++) {
+                    sum += delta[0];
+                    L += ((R << 4)+key[0])^(R+sum)^((R >>> 5)+key[1]);
+                    R += ((L << 4)+key[2])^(L+sum)^((L >>> 5)+key[3]);    
+                }
+                sum = 0;
+                xorer[0] += 1;
+                ciphertext[i] = L^plaintext[i];
+                ciphertext[i+1] = R^plaintext[i+1];
+            }
+            for(int k = 0; k < ciphertext.length; k++) {
+                System.out.print(ciphertext[k] + " ");
+            }
+            System.out.println("");           
         }
         return ciphertext;
     }
@@ -109,25 +160,60 @@ public class TinyE {
         Integer[] plaintext = new Integer[ciphertext.length];
 
         Integer[] delta = convertFromHexStringToInts(DELTA);
-        int sum = delta[0];
+        int sum = delta[0] << 5;
         if(mode == Mode.ECB) {
             for(Integer i = 0; i < ciphertext.length; i+=2) {
                 int L = ciphertext[i];
                 int R = ciphertext[i+1];
                 for(Integer j = 0; j < 32; j++) {
+                    
+                    R -= ((L << 4)+key[2])^(L+(int)sum)^((L >>> 5)+key[3]);
+                    L -= ((R << 4)+key[0])^(R+(int)sum)^((R >>> 5)+key[1]);
                     sum -= delta[0];
-                    R -= ((L << 4)+key[2])^(L+(int)sum)^((L >> 5)+key[3]);
-                    L -= ((R << 4)+key[0])^(R+(int)sum)^((R >> 5)+key[1]);
-    
                 }
-                sum = delta[0];
+                sum = delta[0] << 5;
                 plaintext[i] = L;
                 plaintext[i+1] = R;
             }
         } else if(mode == Mode.CBC) {
+            Integer[] xorer = new Integer[2];
+            xorer[0] = iv[0];
+            xorer[1] = iv[1];
             
+            for(Integer i = 0; i < ciphertext.length; i+=2) {
+                int L = ciphertext[i];
+                int R = ciphertext[i+1];
+                for(Integer j = 0; j < 32; j++) {
+                    
+                    R -= ((L << 4)+key[2])^(L+(int)sum)^((L >>> 5)+key[3]);
+                    L -= ((R << 4)+key[0])^(R+(int)sum)^((R >>> 5)+key[1]);
+                    sum -= delta[0];
+                }
+                sum = delta[0] << 5;
+                plaintext[i] = L^xorer[0];
+                plaintext[i+1] = R^xorer[1];
+                xorer[0] = L;
+                xorer[1] = R;                    
+            }            
         } else {
+            Integer[] xorer = new Integer[2];
+            xorer[0] = iv[0];
+            xorer[1] = iv[1];
             
+            for(Integer i = 0; i < ciphertext.length; i+=2) {
+                int L = xorer[0];
+                int R = xorer[1];
+                for(Integer j = 0; j < 32; j++) {
+                    
+                    R -= ((L << 4)+key[2])^(L+(int)sum)^((L >>> 5)+key[3]);
+                    L -= ((R << 4)+key[0])^(R+(int)sum)^((R >>> 5)+key[1]);
+                    sum -= delta[0];
+                }
+                sum = delta[0] << 5;
+                plaintext[i] = L^ciphertext[i];
+                plaintext[i+1] = R^ciphertext[i+1];
+                xorer[0] += 1;                    
+            }            
         }
         return plaintext;
     }
