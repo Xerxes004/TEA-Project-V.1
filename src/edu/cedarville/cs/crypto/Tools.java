88580d713f1f44774448d53cede87162ab5ea3cf
Wesley Kelly <weskelly@mac.com>
2015-02-21 11:40:59 -0500

    Tools should fully work

--- src/edu/cedarville/cs/crypto/Tools.java
+++ src/edu/cedarville/cs/crypto/Tools.java
@@ -17,10 +17,10 @@ public class Tools {
         for (int i = 0; i < ints.length; i++) {
             int a, b, c, d;
 
-            a = (j + 3 < bs.length ? bs[j + 3] : 0) << 24;
-            b = (j + 2 < bs.length ? bs[j + 2] : 0) << 16;
-            c = (j + 1 < bs.length ? bs[j + 1] : 0) << 8;
-            d = (j < bs.length ? bs[j] : 0);
+            a = (j     < bs.length ? bs[j] : 0) << 24;
+            b = (j + 1 < bs.length ? bs[j + 1] : 0) << 16;
+            c = (j + 2 < bs.length ? bs[j + 2] : 0) << 8;
+            d = (j + 3 < bs.length ? bs[j + 3] : 0);
 
             ints[i] = a | b | c | d;
 
@@ -37,7 +37,7 @@ public class Tools {
      * @return Integer[] array created from input hex string
      */
     public static Integer[] convertFromHexStringToInts(String s) {
-        int numOfHexStrings = s.length() / 8 + (s.length() % 8 != 0 ? 1 : 0);
+        int numOfHexStrings = s.length() / 4 + (s.length() % 4 != 0 ? 1 : 0);
 
         String[] strings = new String[numOfHexStrings];
 
@@ -55,14 +55,15 @@ public class Tools {
                 }
                 strings[i] = str + strings[i];
             }
+            System.out.println("Made: " + strings[i]);
             j += 8;
         }
 
         Integer[] ints = new Integer[numOfHexStrings];
         for (int i = 0; i < strings.length; i++) {
             BigInteger uncipheredInt = new BigInteger(strings[i], 16);
-            ints[strings.length - i - 1] = uncipheredInt.intValue();
-            System.out.println("::" + Integer.toHexString(ints[strings.length - i - 1]));
+            ints[i] = uncipheredInt.intValue();
+            System.out.println("::" + Integer.toHexString(ints[i]));
         }
 
         return ints;
@@ -78,11 +79,11 @@ public class Tools {
         byte[] bytes = new byte[ints.length * 4];
         int j = 0;
         for (int i = 0; i < ints.length; i++) {
-
-            bytes[j + 3] |= ints[i];
-            bytes[j + 2] |= ints[i] >>> 8;
+            
+            bytes[j]     |= ints[i] >>> 24;
             bytes[j + 1] |= ints[i] >>> 16;
-            bytes[j] |= ints[i] >>> 24;
+            bytes[j + 2] |= ints[i] >>> 8;
+            bytes[j + 3] |= ints[i];
 
             j += 4;
         }
@@ -108,7 +109,7 @@ public class Tools {
         System.out.print("Printing ");
         String[] strings = new String[bytes.length];
         for (int i = 0; i < bytes.length; i++) {
-            strings[i] = Integer.toBinaryString(bytes[i]);
+            strings[i] = Integer.toBinaryString(bytes[i] & 0x000f);
             String s = "";
             for (int j = 0; j < 8 - strings[i].length(); j++) {
                 s += "0";
@@ -129,7 +130,7 @@ public class Tools {
     }
 
     public static void main(String[] args) {
-       /* Integer[] testInt = {0xf, 0x00111100, 0x01020304};
+        Integer[] testInt = {0xffff, 0x10111100, 0x01020304};
 
         printIntArrayAsBinary(testInt);
 
@@ -139,11 +140,10 @@ public class Tools {
 
         testInt = convertFromBytesToInts(bytes);
 
-        printIntArrayAsBinary(testInt);*/
+        printIntArrayAsBinary(testInt);
 
         String s = "ae";
         printIntArrayAsBinary(convertFromHexStringToInts(s));
         //System.out.println("Hex string: " + convertFromIntsToHexString(convertFromHexStringToInts(s)));
     }
-
 }
